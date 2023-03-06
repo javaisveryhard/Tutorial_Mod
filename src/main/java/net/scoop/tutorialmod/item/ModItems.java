aff74fe6175f21ec1ec5076ea34acad6a4aba240
Louis Martin <louiszmartin@gmail.com>
2023-03-06 23:14:34 +0000

    Added custom item - Eight ball that outputs to the chat a random number and only works in the main hand -STABLE

--- src/main/java/net/scoop/tutorialmod/item/ModItems.java
+++ src/main/java/net/scoop/tutorialmod/item/ModItems.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.scoop.tutorialmod.TutorialMod;
+import net.scoop.tutorialmod.item.custom.EightBallItem;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -18,6 +19,8 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
     public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> EIGHT_BALL = ITEMS.register("eight_ball",
+            () -> new EightBallItem(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB).stacksTo(1)));
 
 
     public static void register(IEventBus eventBus) {
